3a94c60034e25d17d68742d429e94fce1f9961d5
Hayden Jones <122436575+HaydenRJones@users.noreply.github.com>
2025-07-09 20:02:15 +1200

    Add files via upload

---
+++ htbfrs.py
@@ -0,0 +1,98 @@
+#     ============================================
+#    |  _    _ _______ _      __ _____      ___   |
+#    | | |  | |__   __| |    / _|  __ \    |__ \  |
+#    | | |__| |  | |  | |__ | |_| |__) |___   ) | |
+#    | |  __  |  | |  | '_ \|  _|  _  // __| / /  |
+#    | | |  | |  | |  | |_) | | | | \ \\__ \/ /_  |
+#    | |_|  |_|  |_|  |_.__/|_| |_|  \_\___/____| |
+#    |                                            |
+#     ============================================
+#
+# Hacked together, (but/barely) functional read spliter
+# HJ - 24/05/25
+#
+
+import os
+import time
+import argparse
+import multiprocessing
+import htrs_funcs as htrs
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(
+        prog = 'htbfrs2.py',
+        description = 'Hacked together (but/barely functional) read spliter \nsplit highly concatenated reads from ONT sequencing',
+        epilog = 'version 2.0 - HJ 24/05/25')
+    
+    parser.add_argument('-s', '--sample', 
+                    help = 'Directory to the fastq file to processes',
+                    required = True)
+    
+    parser.add_argument('-o', '--output', 
+                    help = 'Directory to the save data to.',
+                    default = f'{os.getcwd()}/output')
+    
+    parser.add_argument('-B', '--blastn_path', 
+                    help = 'Directory to the blastn executable if not in $PATH',
+                    default = 'blastn')
+    
+    parser.add_argument('-N', '--split_number', 
+                    help = 'How many reads for breaking up the data.\nDefault value is 25,000',
+                    default = 25000)
+    
+    parser.add_argument('-t', '--threads', 
+                    help = 'Number of threads to use for BLAST searches.\nDefault value is 4',
+                    default = 4)
+    
+    parser.add_argument('-W', '--word_size', 
+                    help = 'The word size to use for BLAST searches\nDefault value is 10',
+                    default = 10)
+    
+    args = parser.parse_args()
+    
+    start_time = time.time()
+    
+    # make output folder
+    if not os.path.exists(args.output):
+        print('making output folder')
+        os.makedirs(args.output)
+    
+    folder_time = time.time()
+    
+    # break up the file, and convert it into fasta. Return a list of fasta files to blast
+    print('splitting file')
+    file_list = htrs.preprocess_data(args.sample, args.split_number, args.output)
+    
+    file_time = time.time()
+    
+    print('running blast')
+    for file in file_list:
+        
+        htrs.blast(args.blastn_path, 'ref/barcodes', file, args.word_size, args.threads)
+        #htrs.blast('blastn', 'ref/barcodes', file, 10, 4)
+    
+    blast_time = time.time()
+    
+    print('splitting reads')
+    with multiprocessing.Pool(args.threads) as p:
+        
+        p.map(htrs.split_reads, file_list)
+        
+    end_time = time.time()
+    
+    print(f'''
+          making folders   :  {round(folder_time - start_time, 3)}
+          splitting files  :  {round(file_time - folder_time, 3)}
+          blasting reads   :  {round(blast_time - file_time, 3)}
+          splitting reads  :  {round(end_time - blast_time, 3)}
+          --------------------{'-' * len(str(round(end_time - start_time, 3)))}
+          total runtime    :  {round(end_time - start_time, 3)}
+          ''')
+    
+    
+    
+    
+    
+    
+    
---
+++ htrs_funcs.py
@@ -0,0 +1,96 @@
+# Hacked together, (but/barely) functional read spliter
+# HJ - 24/05/25
+#
+from glob import glob
+import subprocess
+import pandas as pd
+from Bio import SeqIO
+from collections import defaultdict
+
+# def setup_dir(target_dir):
+
+#     print()
+
+def preprocess_data(target, file_size, output_folder):
+    
+    # split the file 
+    command = f"split -l {file_size * 4} {target} {output_folder}/{target.split('/')[-1]}."
+    subprocess.run(command, shell = True)
+    
+    for file in glob(f'{output_folder}/*'):
+        
+        command = f"seqkit fq2fa {file} -o {file}.fa"
+        subprocess.run(command, shell = True)
+    
+    return(glob(f'{output_folder}/*.fa'))
+    
+def blast(blastn_path, database, target, word_size, num_threads):
+    
+    # Command to run blast, and a header to append to the output tsv file. 
+    # We probably don't need to do this but it makes it a bit easier to parse later
+    command = f'{blastn_path} -db {database} -query {target} -word_size {word_size} -num_threads {num_threads} -out {target}.out -outfmt "6 qseqid sseqid qlen pident length mismatch gapopen qstart qend sstart send evalue bitscore"'
+    header = 'qseqid	sseqid	qlen	pident	length	mismatch	gapopen	qstart	qend	sstart	send	evalue	bitscore'
+    
+    subprocess.run(command, shell = True)
+    subprocess.run(f'cat header {target}.out > {target}.tsv', shell = True)
+    
+def split_reads(target):
+    cutoff = 18
+    
+    # Read in the blast tsv, and filter based on the hit length (alternatives might be eval or bitscore!)
+    df = pd.read_csv(f'{target}.tsv', sep = '\t')
+    df = df.loc[df['length'] >= cutoff]
+    
+    # Save the hit table as a dict for faster access. Delete the df afterwards to save memory
+    hitDict = defaultdict(list)
+    for _, row in df.iterrows():
+        hitDict[row['qseqid']].append((row['qstart'], row['qend'], row['sstart'], row['send']))
+    del(df)
+    
+    # Make an empty string to save our split sequences to
+    splitSeqs = ''
+    
+    # Loop over each read in the file
+    for record in SeqIO.parse(f"{'.'.join(target.split('.')[:-1])}", 'fastq'):
+        
+        # Get a bunch of info relating to the current read
+        name = str(record.id)
+        seq  = str(record.seq)
+        qual = ''.join([chr(score + 33) for score in record.letter_annotations['phred_quality']]) 
+    
+        # Check to see if the read exists in our hit dicionary
+        # If it does processes the readm if not write the read and go to the next
+        hits = hitDict.get(name)
+        if hits:
+            
+            # Make an empty list to store the split coordinates
+            # Loop over each one, storing them and then sorting so they are in order
+            # Finally save to a list of regions (including the start and stop) that need to be split
+            splits = []
+            
+            for i in range(len(hits)):
+                
+                splits.append(hits[i][hits[i][2:].index(min(hits[i][2:]))])
+            
+            splits.sort()
+            # Cant remember what this was for so just going to remove it for now...
+            #regions = [0] + splits + [len(seq)]
+            
+            # Go pair by pair through splits, and use those values to break up the read.
+            # Saving it to our string as we go
+            for i in range(len(splits) - 1): # this was (0, len(splits) - 1), so hopefully it wont break?
+                
+                span = [splits[i], splits[i + 1]]
+                
+                newName = f'{name}_{i}'
+                newSeq  = f'{seq[span[0]:span[1]]}'
+                newQual = f'{qual[span[0]:span[1]]}'
+                
+                splitSeqs += f'@{newName}\n{newSeq}\n+\n{newQual}\n'
+             
+        else:
+            splitSeqs += f'@{name}\n{seq}\n+\n{qual}\n'
+
+    # Save the file to the disk after all reads are split
+    with open(f"{'.'.join(target.split('.')[:-1])}_split.fastq", 'w') as f: #.fastq
+        f.writelines(splitSeqs)
